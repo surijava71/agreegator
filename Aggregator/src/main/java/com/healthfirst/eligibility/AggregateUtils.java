277ff91bcd6d9b75aafcb3409870def7ea5f2d48
surijava71 <98726251+surijava71@users.noreply.github.com>
2022-02-06 17:16:39 -0600

    Update AggregateUtils.java

--- Aggregator/src/main/java/com/healthfirst/eligibility/AggregateUtils.java
+++ Aggregator/src/main/java/com/healthfirst/eligibility/AggregateUtils.java
@@ -20,72 +20,42 @@ import com.amazonaws.services.dynamodbv2.model.AttributeValueUpdate;
 import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
 import com.amazonaws.services.dynamodbv2.model.UpdateItemRequest;
 import com.amazonaws.services.dynamodbv2.model.UpdateItemResult;
+mport com.google.gson.JsonObject;
 
- 
 public class AggregateUtils {
-  
- private DynamoDB dynamoDb;
- private String DYNAMO_DB_TABLE_NAME = "Employee";
- private Regions REGION = Regions.US_EAST_1;
- 
- 
- private static final Logger logger = LogManager.getLogger(HandlerMSK.class);
- 
- private AmazonDynamoDBClient client;
-  public void initDynamoDbClient() {
-	
-	  
-  client = new AmazonDynamoDBClient();
-  logger.info("Init Dynamo DB");
-  client.setRegion(Region.getRegion(REGION));
-  this.dynamoDb = new DynamoDB(client);
- }
-  
- public PutItemOutcome aggregateEvent(HashMap<String,Object> event) {
-	 logger.info("Persist Dynamo DB1");
-	 Table table = dynamoDb.getTable(DYNAMO_DB_TABLE_NAME);
-	 
-	 
-	 Map<String,AttributeValue> attributeValues = new HashMap<>();
-	    attributeValues.put("hf_member_num_cd",new AttributeValue().withS((String)event.get("hf_member_num_cd")));
-	  /* 
-	    UpdateItemRequest updateItemRequest = new UpdateItemRequest()
-	        .withTableName("Eligibility")
-	        .addKeyEntry("hf_member_num_cd",new AttributeValue().withS((String)event.get("hf_member_num_cd")))
-	        .addAttributeUpdatesEntry("first_nm",new AttributeValueUpdate().withValue(new AttributeValue().withS((String)event.get("first_nm")))
-	        		.addAttributeUpdatesEntry("first_nm",new AttributeValueUpdate().withValue(new AttributeValue().withS((String)event.get("first_nm")))
-	        		        
-	            
-	        		
-	        		);
-	        */
-	        
-	 
-	/* 
-	 attributeValues.put("email",new AttributeValue().withS(email));
-	    attributeValues.put("fullname",new AttributeValue().withS(fullName));
 
-	    UpdateItemRequest updateItemRequest = new UpdateItemRequest()
-	        .withTableName("Eligibility");
-	        .addKeyEntry("hf_member_num_cd",new AttributeValue().withS(email))
-	        .addAttributeUpdatesEntry("fullname",
-	            new AttributeValueUpdate().withValue(new AttributeValue().withS(fullName)));
+	private DynamoDB dynamoDb;
+	private String DYNAMO_DB_TABLE_NAME = "Employee";
+	private Regions REGION = Regions.US_EAST_1;
 
-	    UpdateItemResult updateItemResult = client.updateItem(updateItemRequest);
-	    
-	    
-	 
-	 logger.info(table.getTableName());
-	 logger.info("Persist Dynamo DB2");
-	 PutItemOutcome outcome = table.putItem(new PutItemSpec().withItem(
-    new Item().withNumber("empId", employee.getEmpId())
-               .withString("firstName", employee.getFirstName())
-               .withString("lastName", employee.getLastName())));
-  logger.info("after Persist Dynamo DB");*/
-  //return outcome;
-	    return null;
- }
- 
- 
- 
+	private static final Logger logger = LogManager.getLogger(AggregateUtils.class);
+	private String PK = "hf_member_num_cd";
+
+	DynamoDBUtils dbUtils = null;
+
+	public void initDynamoDbClient() {
+
+		dbUtils = new DynamoDBUtils();
+	}
+
+	public String aggregate(JsonObject payload) {
+		DynamoDBUtils dbUtils = new DynamoDBUtils();
+		Item it = null;
+		it = dbUtils.getItem(payload.get(PK).getAsString());
+		if (it == null)
+			it = new Item(); // This is to start new item.
+
+		List<Item> itL = new ArrayList<Item>();
+		itL.add(it);
+		payload.keySet().stream().forEach(key -> {
+			Item iit = itL.get(0);
+			logger.info(key);
+			iit = iit.withString(key, payload.get(key).getAsString());
+			itL.set(0, iit);
+		});
+		dbUtils.updateItem(itL.get(0));
+		
+		return itL.get(0).toJSON();
+	}
+
 }
